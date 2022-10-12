3ee9e3f62e03f972231a79e92244dc8bbdef58a5
Damian25121997 <damiancaetano25@gmail.com>
2022-10-12 15:16:33 -0300

    fix test file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""""Test of file storage"""
+
+
+import unittest
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import json
+
+file = "file.json"
+
+
+class TestFileStorage(unittest.TestCase):
+    """Testing for FileStorage class"""
+
+    def testFile(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def testObj(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def testAll(self):
+
+        dictionary = storage.all()
+        self.assertEqual(type(dictionary), dict)
+
+    def testNew(self):
+
+        new = storage.all().copy()
+        storage.new(BaseModel())
+        self.assertNotEqual(new, storage.all())
