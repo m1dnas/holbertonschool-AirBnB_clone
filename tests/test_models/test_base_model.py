13fa9d16cac515f36ae6edec0c5cd13fe7cb96aa
Felipe Sandim <felipesandimguliermone@gmail.com>
2022-10-13 10:08:40 -0300

    Add comments

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,16 +8,18 @@ import unittest
 import json
 from models.base_model import BaseModel
 from models import storage
-import os
 
 class TestBaseModel(unittest.TestCase):
-
+"""Class of TestBaseModel"""
+    
     def test_1(self):
+    """Test n°1: BaseModel - self.created_at & self.id"""
         b = BaseModel()
         self.assertEqual(type(b.id), str)
         self.assertEqual(type(b.created_at), datetime)
         
     def test_2(self):
+    """Test n°2: BaseModel - self.updated_at"""
         b = BaseModel()
         b_update = b.updated_at
         sleep(0.04)
@@ -25,10 +27,12 @@ class TestBaseModel(unittest.TestCase):
         self.assertLess(b_update, b.updated_at)
     
     def test_3(self):
+    """Test n°3: BaseModel - __str__(self)"""
         b = BaseModel()
         self.assertEqual(str(b), "[{}] ({}) {}".format(b.__class__.__name__, b.id, b.__dict__))
 
     def test_4(self):
+    """Test n°4: BaseModel - to_dict()"""
         b = BaseModel()
         b_dict = dict(b.__dict__)
         b_dict["__class__"] = "BaseModel"
@@ -36,7 +40,8 @@ class TestBaseModel(unittest.TestCase):
         b_dict["updated_at"] = b_dict["updated_at"].isoformat()
         self.assertDictEqual(b_dict, b.to_dict())
 
-    def testSave(self):
+    def test_5(self):
+    """Test n°5: BaseModel - save()"""
         b = BaseModel()
         b.save()
         with open('file.json', 'r') as f:
