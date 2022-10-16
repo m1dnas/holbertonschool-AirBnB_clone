1e9c5ae7af8417bef82342ad11029dadbb8887fe
m1dnas <felipesandimguliermone@gmail.com>
2022-10-16 11:50:12 -0700

    user test fixed

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,18 +9,18 @@ from models.user import User
 class TestUser(unittest.TestCase):
     """All the tests for the user class"""
 
-    def email_test(self):
+    def test_email(self):
         """Test n°1: User - user.email"""
         self.assertEqual(str, type(User.email))
 
-    def password_test(self):
+    def test_password(self):
         """Test n°2: User - user.password"""
         self.assertEqual(str, type(User.password))
 
-    def firstname_test(self):
+    def test_firstname(self):
         """Test n°3: User - user.first_name"""
         self.assertEqual(str, type(User.first_name))
 
-    def lastname_test(self):
+    def test_lastname(self):
         """Test n°4: User - user.last_name"""
         self.assertEqual(str, type(User.last_name))                                   
